5046e004e03c12c91b44a5139c3eabfc117f5b29
ziwei11 <ziwei11@ualberta.ca>
2021-09-04 18:41:34 -0600

    lab1

---
+++ lab_1.py
@@ -0,0 +1,13 @@
+# CMPUT404 Lab1 Fall 2021
+# Vicky Zhao
+
+import requests
+
+requests_version = requests.__version__
+print('Requests version is ' + requests_version)
+
+Google_homepage = requests.get('https://www.google.com')
+print('Google home page is ' + str(Google_homepage))
+
+source_code = requests.get('https://github.com/ziwei11/CMPUT404_Lab1')
+print(source_code.text)
